f6393d3b16b5322ae9f789f0adc148b78f865b27
Kadir Niyazi AÇAR <kadirnacar@kadirnacar.com>
2024-12-13 20:22:23 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+class Uye {
+  Adi: string = "";
+  Soyadi: string = "";
+  Eposta: string = "";
+}
 
 function App() {
+  // const [yeniUye, setYeniUye] = useState(new Uye());
+  let yeniUye = new Uye();
+  yeniUye.Adi = "Kadir";
+  function gosterBas() {
+    // yeniUye.Adi = "kjncksncdsknsd";
+    console.log(yeniUye);
+  }
+
   return (
     <div className="App">
+      <div>
+        <div>
+          <label></label>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Adı:</label>
+        <input
+          onChange={(event) => {
+            yeniUye.Adi = event.target.value;
+          }}
+        ></input>
+        <label>Soyadı:</label>
+        <input
+          onChange={(event) => {
+            yeniUye.Soyadi = event.target.value;
+          }}
+        ></input>
+        <label>E-Posta:</label>
+        <input
+          onChange={(event) => {
+            yeniUye.Eposta = event.target.value;
+          }}
+        ></input>
+        <button onClick={gosterBas}>Göster</button>
       </header>
     </div>
   );
